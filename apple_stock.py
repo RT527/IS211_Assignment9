ce3634cf323c4a87e4bbe25d10e311e9f2989e00
“RT527” <rafi.talukder7@gmail.com>
2025-10-28 06:54:14 -0400

    Starting apple_stock.py

---
+++ apple_stock.py
@@ -0,0 +1,9 @@
+# Rafi Talukder Assignment_9
+# URL scraped (data source): https://query1.finance.yahoo.com/v8/finance/chart/AAPL?interval=1d&range=1mo
+"""---------------------------------------------------------------------------------"""
+# This avoids HTML 404s by requesting structured JSON instead, then parsing it in Python.
+"""---------------------------------------------------------------------------------"""
+
+
+if __name__ == "__main__":
+    main()
--- scraper_two.py
+++
@@ -1,5 +0,0 @@
-# Rafi Talukder Assignment_9
-
-"""---------------------------------------------------------------------------------"""
-if __name__ == "__main__":
-    print("Running scrapper two...")
